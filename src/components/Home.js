355ebc5d2472096fa506eb679e67a66fe55882dd
Anil <anil_gurung15@yahoo.com>
2020-12-28 11:03:49 -0800

    added css for buttons and made it responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,6 +17,7 @@ import '../App.css'
 import HeroSection from './HeroSection'
 
 export const Home = () => {
+    document.getElementsByTagName("video")[0].playbackRate = 0.5
     return (
         <>
             <HeroSection />
