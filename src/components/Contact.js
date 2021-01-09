f1557c782fc48ae19ab7c9e6cd163027945d8a29
Anil <anil_gurung15@yahoo.com>
2021-01-09 14:02:04 -0800

    added form to contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,66 @@
-import React from 'react'
+
+import React, { Component } from 'react'
 import './Contact.css'
 
-function Contact() {
-    return (
-        <div className ="contact__container">
-            <img src='images/img-9.jpg'/>
-            <h1>NICE TO MEET YOU ! </h1>
-        </div>
-    )
+export class Contact extends Component {
+    state = {
+        name: "",
+        email: "",
+        message: ""
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        if(this.state.name !== " " && this.state.email !== " " && this.state.message !== " "){
+            this.setState({
+                name: "",
+                email: "",
+                message: ""
+            })
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className ="contact__container">
+                <img src='images/img-9.jpg'/>
+                <h1>Nice to meet you ! Lets Connect </h1>
+                <div align="center">
+                    <form onSubmit={this.handleSubmit.bind(this) } >
+                        <div>
+                            <label htmlFor="name"></label>
+                            <input type="text" name="name" id="textboxid" class="form-control" placeholder="Name" value={this.state.name} onChange={this.handleChange} />
+                        </div> <br />
+
+
+                        <div>
+                            <label htmlFor="email"> </label>
+                            <input type="text" name="email" id="textboxid" class="form-control" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
+                        </div><br />
+
+                        <div>
+                            <label htmlFor="message"></label>
+                            {/* <input type="textarea" name="message" id="textboxidmsg" class="form-control" placeholder="Message" value={this.state.message} onChange={this.handleChange} /> */}
+                            <textarea id="textboxidmsg" placeholder="Message" value={this.state.message} onChange={this.handleChange}></textarea>
+                        </div><br />
+
+                        <br />
+
+                        <input type="submit" value="Send" class="btn btn-primary" />
+
+                    </form>
+
+                </div>
+
+            </div>
+        )
+    }
 }
 
 export default Contact
