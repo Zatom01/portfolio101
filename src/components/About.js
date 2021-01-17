219b9b51ae022ea674a4f802aea58f45e4a32be3
Anil <anil_gurung15@yahoo.com>
2021-01-16 16:04:41 -0800

    added flex css property for divs

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,19 @@ function About() {
             <img src="images/img-home.jpg"/>
             <h1>This is about page</h1>
 
+            <div class="flex-container">
+
+                <div class="flex-child magenta">
+                    Flex Column 1
+                </div>
+
+                <div class="flex-child green">
+                    Flex Column 2
+                </div>
+
+            </div>
+
+
         </div>
     )
 }
