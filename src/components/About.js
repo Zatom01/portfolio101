17f88bc094309cadca3f7bfa744b905068397702
Anil <anil_gurung15@yahoo.com>
2021-01-16 16:21:17 -0800

    populated the divs with texts in About component

--- src/components/About.js
+++ src/components/About.js
@@ -5,16 +5,17 @@ function About() {
     return (
         <div className="about__container">
             <img src="images/img-home.jpg"/>
-            <h1>This is about page</h1>
+
 
             <div class="flex-container">
 
                 <div class="flex-child magenta">
-                    Flex Column 1
+                    Hi, I am Neal living in Reno, NV. I love music, data and product engineering.
                 </div>
 
                 <div class="flex-child green">
-                    Flex Column 2
+                Full Stack Web Developer with a background in Computer Science. A self-motivated, creative, and organized professional with experience in Ruby on Rails, React + Redux, and JavaScript based programming. My love of coding and seeing the positive impact it has on everyday life began while I was growing up in Nepal. I bring strong collaboration and communication skills that allow me to work effectively with people from a variety of backgrounds.
+
                 </div>
 
             </div>
