bb2c15c2324424ccbfa5de2d34c7bd07af0e2022
Anil <anil_gurung15@yahoo.com>
2021-01-19 10:20:26 -0800

    changed font colors

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../App.css'
-import {Button} from "./Button"
+// import {Button} from "./Button"
 import './HeroSection.css'
 import {Link} from 'react-router-dom'
 
@@ -17,10 +17,10 @@ function HeroSection() {
             <div className="hero-btns">
                 {/* <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large" onClick= {this.aboutRoute}>About</Button>
                 <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large" onClick= {event =>  window.location.href='/resume'}>Projects<i className='far far-play-circle' /></Button> */}
-                <Link to="/about"><h1>About</h1></Link>
+                <Link className="link-font" to="/about" ><h1>About</h1></Link>
 
                 <br></br>
-                <Link to="/resume"><h1>Projects</h1></Link>
+                <Link className="link-font" to="/resume" ><h1>Projects</h1></Link>
             </div>
 
             {/* <div class="flex-container">
