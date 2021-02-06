a74f576c9f25e2f9ff2af5b7d95ba77564c22c74
Anil <anil_gurung15@yahoo.com>
2021-02-06 14:49:07 -0800

    used react hooks to project boxes

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Project.css"
 
 function Project() {
+    const [projectState, changeState] = useState({
+        activeObject: null ,
+        objects: [{ id: 1}, { id: 2}, { id: 3}, { id: 4}, { id: 5} ]
+    });
+
+    function toggleActive(index){
+        changeState({...projectState, activeObject: projectState.objects[index]});
+    }
+
+    function toggleActive(index){
+        changeState({...projectState,activeObject: projectState.objects[index]})
+    }
+
     return (
         <div classsName= "project__container">
             <img src="images/img-project.jpeg"/>
+            {projectState.objects.map((elements,index) => (
+                <div key={index} className ="box inactive" onClick ={()=> {toggleActive(index)}}></div>
+            )
+            )}
+
         </div>
     )
 }
