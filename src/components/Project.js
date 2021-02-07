8dff902460a1d8453f03e9ca800e0e9c8cbaea50
Anil <anil_gurung15@yahoo.com>
2021-02-07 09:53:13 -0800

    updated css for thumbnails

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,16 +4,12 @@ import "./Project.css"
 function Project() {
     const [projectState, changeState] = useState({
         activeObject: null ,
-        objects: [{ id: 1, name: "CLI",image:"images/img-nokogiri.jpeg"},
-         { id: 2, name:"Sinatra",image:"images/img-sinatra.jpeg"},
-        { id: 3, name:"Rails",image:"images/img-rubyrails.jpg"},
-         { id: 4, name:"Javascript",image:"images/img-js.jpg"},
-          { id: 5, name:"React",image:"images/img-reactredux.jpeg"}]
+        objects: [{ id: 1, name: "CLI",image:"images/img-nokogiri.jpeg", description:"About Project"},
+         { id: 2, name:"Sinatra",image:"images/img-sinatra.jpeg", description:"About Project"},
+        { id: 3, name:"Rails",image:"images/img-rubyrails.jpg", description:"About Project"},
+         { id: 4, name:"Javascript",image:"images/img-js.jpg", description:"About Project"},
+          { id: 5, name:"React",image:"images/img-reactredux.jpeg", description:"About Project"}]
     });
-
-    function toggleActive(index){
-        changeState({...projectState, activeObject: projectState.objects[index]});
-    }
 
     function toggleActive(index){
         changeState({...projectState,activeObject: projectState.objects[index]})
@@ -21,6 +17,7 @@ function Project() {
 
     function toggleActiveStyles(index){
         if(projectState.objects[index] === projectState.activeObject){
+
             return "box active";
         }
         else{
@@ -34,11 +31,13 @@ function Project() {
             <h1>This page is still developing!</h1>
             <img src="images/img-project.jpeg" alt="background"/>
             {projectState.objects.map((elements,index) => (
-                <div key={index} className ={toggleActiveStyles(index)} onClick ={()=> {toggleActive(index)}}>
-                    <h2>{elements.name}</h2>
-                    <img src={elements.image} alt="" />
+                <>
+                    <div key={index} className ={toggleActiveStyles(index)} onClick ={()=> {toggleActive(index)}}>
+                        <h2>{elements.name}</h2>
+                        <img src={elements.image} alt="" />
+                    </div>
 
-                </div>
+                </>
             )
             )}
 
