7649e17869357a8dcc84a96a3d7757fcd107b807
Anil <anil_gurung15@yahoo.com>
2021-02-15 13:11:44 -0800

    added project description div for active objects

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,13 +4,13 @@ import "./Project.css"
 function Project() {
     const [projectState, changeState] = useState({
         activeObject: null ,
-        objects: [{ id: 1, name: "CLI",image:"images/img-nokogiri.jpeg", description:"About Project", github:"https://github.com/Zatom01/CLI-Data-Gem-Portfolio-Project"},
-         { id: 2, name:"Sinatra",image:"images/img-sinatra.jpeg", description:"About Project",github:"https://github.com/Zatom01/Sinatra-portfolio-project"},
-        { id: 3, name:"Rails",image:"images/img-rubyrails.jpg", description:"About Project",github:"https://github.com/Zatom01/Rails-portfolio-project"},
-         { id: 4, name:"Javascript",image:"images/img-js.jpg", description:"About Project",github:"https://github.com/Zatom01/destination-review-js-frontend"},
-          { id: 5, name:"React",image:"images/img-reactredux.jpeg", description:"About Project",github:"https://github.com/Zatom01/ReactFrontendDeploy-LandlordsApp"},
-          { id: 6, name:"Python",image:"", description:"About Project",github:"https://github.com/Zatom01/ComputerVisionMediaController-SeniorProject2019"},
-          { id: 7, name:"Java",image:"", description:"About Project"}
+        objects: [{ id: 1, name: "CLI",image:"images/img-nokogiri.jpeg", project_name: "Webscraping", technology_used: "Ruby, Nokogiri, open-URI, lolize", description:"About Project", github:"https://github.com/Zatom01/CLI-Data-Gem-Portfolio-Project",video_demo:"https://youtu.be/oyeCBNCKCMU"},
+         { id: 2, name:"Sinatra",image:"images/img-sinatra.jpeg",project_name: "producers-blog",technology_used:"Ruby, bcrypt", description:"About Project",github:"https://github.com/Zatom01/Sinatra-portfolio-project",video_demo:"https://youtu.be/T0QN3EYgWfU"},
+        { id: 3, name:"Rails",image:"images/img-rubyrails.jpg",project_name: "producers-blog",technology_used:"Rails, bcrypt, omniauth",   description:"About Project",github:"https://github.com/Zatom01/Rails-portfolio-project",video_demo:"https://youtu.be/F8exfHLkdSA"},
+         { id: 4, name:"Javascript",image:"images/img-js.jpg", project_name: "Destination review",technology_used:"Javascript, Rails",  description:"About Project",github:"https://github.com/Zatom01/destination-review-js-frontend",video_demo:"https://js-frontend-destination-review.netlify.app/"},
+          { id: 5, name:"React",image:"images/img-reactredux.jpeg",project_name: "Landlord Management app",technology_used:"React, Redux, Rails",   description:"About Project",github:"https://github.com/Zatom01/ReactFrontendDeploy-LandlordsApp",video_demo:"https://react-frontend-deploy.netlify.app"},
+          { id: 6, name:"Python",image:"", description:"About Project",project_name: "Computer Vision Media Controller",technology_used:"Python, OpenCV, Machine Learning",  github:"https://github.com/Zatom01/ComputerVisionMediaController-SeniorProject2019",video_demo:"https://www.youtube.com/watch?v=RbxZ-CXqGhE"},
+          { id: 7, name:"Java",image:"", project_name: "Regression",technology_used:"Java",  description:"About Project", github:"needSomeURL",video_demo:"Coming Soon"}
         ]
     });
 
@@ -45,6 +45,17 @@ function Project() {
                 )
                 )}
 
+            </div>
+            <div className="description__container">
+                    <ul>
+                        <li><h3>Project Name:</h3> <h4>{projectState.activeObject === null ? "" : projectState.activeObject.project_name}</h4> </li>
+                        <li><h3>Technology Used:</h3> <h4>{projectState.activeObject === null ? "" : projectState.activeObject.technology_used}</h4></li>
+                        <li><h3>Project Description:</h3> <h4>{projectState.activeObject === null ? "" : projectState.activeObject.description}</h4></li>
+                        <li><h3>Github:</h3> <h4>{projectState.activeObject === null ? "" : projectState.activeObject.github}</h4></li>
+                        <li><h3>Video/Demo: </h3> <h4>{projectState.activeObject === null ? "" : projectState.activeObject.video_demo}</h4></li>
+
+
+                    </ul>
             </div>
 
 
